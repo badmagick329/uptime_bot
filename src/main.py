f642ab26f964b3a6476f82383eccc8ef35ff09c4
badmagick329 <uzairfarooqi@gmail.com>
2024-09-12 00:38:33 +0100

    feat: add email notification

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 import httpx
 
 from consts import UPTIME_FILE
+from sender import SMTPSender
 
 logging.basicConfig(
     level=logging.INFO,
@@ -20,25 +21,51 @@ class Result:
     address: str
     status: int
 
+    def __str__(self) -> str:
+        return f"{self.address} {self.status}"
+
 
 def main():
     with open(UPTIME_FILE, "r", encoding="utf-8") as f:
         uptime_checks = json.load(f)
 
+    sender = SMTPSender()
+
     for check in uptime_checks:
         addresses = check.get("addresses")
         email = check.get("email")
-        results = []
-        for address in addresses:
-            print(f"Checking {address}")
-            try:
-                results.append(check_status(address))
-            except httpx.TimeoutException:
-                results.append(Result(address, 408))
-            except Exception:
-                results.append(Result(address, 500))
-        for result in results:
+        results = check_addresses(addresses)
+        failed_checks = get_failed_checks(results)
+        if failed_checks:
+            failed_checks_str = "\n".join([str(r) for r in failed_checks])
+            sender.send_mail(
+                email,
+                "Uptime check failed",
+                f"{failed_checks_str}",
+            )
+
+
+def get_failed_checks(results: list[Result]) -> list[Result]:
+    failed_checks = []
+    for result in results:
+        if result.status != 200:
+            logging.error(result)
+            failed_checks.append(result)
+        else:
             logging.info(result)
+    return failed_checks
+
+
+def check_addresses(addresses: list[str]) -> list[Result]:
+    results: list[Result] = []
+    for address in addresses:
+        try:
+            results.append(check_status(address))
+        except httpx.TimeoutException:
+            results.append(Result(address, 408))
+        except Exception:
+            results.append(Result(address, 500))
+    return results
 
 
 def check_status(address: str) -> Result:
